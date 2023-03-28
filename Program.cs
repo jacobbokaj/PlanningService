6d840a2bbba726f2efa6cb299eeacd77da8e9099
Jacob <jacobvestergaard@hotmail.dk>
2023-03-28 10:07:01 +0200

    Final xD

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
 using PlanningService;
+using NLog;
+using NLog.Web;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-       services.AddHostedService<Worker>();
-    })
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+try
+{
+    IHost host = Host.CreateDefaultBuilder(args)
+
+        .ConfigureServices(services =>
+        {
+        services.AddHostedService<Worker>();
+        })
+        .ConfigureLogging(logging =>{
+            logging.ClearProviders();
+        }).UseNLog()
     .Build();
 
-
-host.Run();
+    logger.Info("HejHej");
+    host.Run();   
+}
+catch (System.Exception ex)
+{
+    
+    logger.Error(ex,"Stopped program because of exception");
+    throw;
+}finally{
+    NLog.LogManager.Shutdown();
+}
